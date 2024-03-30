88e120e1474675f5e1135df16344bc344accd1b8
KangJeongTaek <dfgddjd@naver.com>
2024-03-31 00:36:56 +0900

    미노 회전

--- simple_tetris/src/mino/Mino_L1.java
+++ simple_tetris/src/mino/Mino_L1.java
@@ -21,5 +21,62 @@ public class Mino_L1 extends Mino{
         b[3].y = b[0].y + Block.SIZE;
 
     }
+    public void getDirection1(){
+        //ㅇ
+        //ㅇ
+        //ㅇㅇ
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y - Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+
+        updateXY(1);
+
+    }
+    public void getDirection2(){
+        //
+        // ㅇ ㅇ ㅇ
+        // ㅇ
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x + Block.SIZE;
+        tempB[1].y = b[0].y ;
+        tempB[2].x = b[0].x - Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y + Block.SIZE;
+        updateXY(2);
+    }
+    public void getDirection3(){
+        //ㅇ ㅇ
+        //   ㅇ
+        //   ㅇ
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[2].x = b[0].x;
+        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[3].x = b[0].x - Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE;
+        updateXY(3);
+    }
+    public void getDirection4(){
+        //      ㅇ
+        //ㅇ ㅇ ㅇ        
+        tempB[0].x = b[0].x;
+        tempB[0].y = b[0].y;
+        tempB[1].x = b[0].x - Block.SIZE;
+        tempB[1].y = b[0].y ;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
+        tempB[3].x = b[0].x + Block.SIZE;
+        tempB[3].y = b[0].y - Block.SIZE; 
+        updateXY(4);
+    }
 
 }
